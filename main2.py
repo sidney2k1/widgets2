c245935dd2be264d040641b74b30e4f14c4a9312
sidney2k1 <sidsingla02@gmail.com>
2024-12-11 17:42:20 +0530

    main2

---
+++ main2.py
@@ -0,0 +1,30 @@
+from tkinter import *
+root=Tk()
+root.title("Email site")
+root.geometry("400x400")
+frame=Frame(master=root,height=200,width=350,bg="dark blue")
+lbl1=Label(frame,text="name",bg="black",fg="white",width=12)
+lbl2=Label(frame,text="Email id",bg="black",fg="white",width=12)
+lbl3=Label(frame,text="Password",bg="black",fg="white",width=12)
+emailentry=Entry(frame)
+nameentry=Entry(frame)
+Passwordentry=Entry(frame,show="*")
+def display():
+    name=nameentry.get()
+    greet="hey "+name
+    message="\nCongratulation for your new account"
+    textbox.insert(END,greet)
+    textbox.insert(END,message)
+textbox=Text(bg="light blue",fg="black")
+btn=Button(text="Create account",command=display,bg="red")
+frame.place(x=20,y=0)
+lbl1.place(x=20,y=20)
+nameentry.place(x=150,y=20)
+lbl2.place(x=20,y=80)
+emailentry.place(x=150,y=80)
+lbl3.place(x=20,y=140)
+Passwordentry.place(x=150,y=140)
+btn.place(x=130,y=210)
+textbox.place(y=310)
+root.mainloop()
+
